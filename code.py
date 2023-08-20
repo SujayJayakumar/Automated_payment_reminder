f1046428c995790e38da3a07c567ae73d9bd6e03
SujayJayakumar <113993766+SujayJayakumar@users.noreply.github.com>
2023-08-20 23:15:21 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,12 @@
 
 import os
+
 from twilio.rest import Client
+
 import keys
+
 import pandas as pd
+
 
 def process_excel_sheet(file_path):
     try:
@@ -14,17 +18,18 @@ def process_excel_sheet(file_path):
             # Get the value of the I column (column index 8)
             status = row.iloc[8]
 
-            # Check if the value in the I column is "Unpaid"
+            # Check if the value in the I column is " Unpaid "
             if status == "Unpaid":
                 # Get the value in the H column (column index 7)
                 if row.iloc[7]=="-":
                     target_no = "number not available"
+                    
                 else:
                     target_no = "+91"+str(int(row.iloc[7]))
 
                     #twilio code to send msg
                     ct = Client(keys.account_sid, keys.auth_token)
-                    ct.messages.create(body= "You have a pending payment towards layout mantainance,\n Make sure to complete the same within the end of this month.\n -RWA",from_=keys.twilio_number,to=target_no)
+                    ct.messages.create(body= " You have a pending payment towards layout mantainance,\n Make sure to complete the same within the end of this month.\n -RWA",from_=keys.twilio_number,to=target_no)
 
 
                 print(f"Unpaid: {target_no}")
@@ -33,6 +38,7 @@ def process_excel_sheet(file_path):
 
     except Exception as e:
         print("Error:", e)
+
 
 # Replace 'path/to/your/excel_file.xlsx' with the actual path to your Excel file
 excel_file_path = "D:\SUJAY\Daily Coding Problems\RoyalSheltersProject\RWA-PaidMemberListAsOnApril23.xlsx"
