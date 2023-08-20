719f5cda3f959ee564f11d2318c2c4773e8543bc
SujayJayakumar <113993766+SujayJayakumar@users.noreply.github.com>
2023-08-20 23:17:00 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
-account_sid = 'SID provided on twilio account after registering'
-auth_token = 'unique token from twilio'
+account_sid = 'enter SID provided on twilio account after registering'
+auth_token = 'enter unique token from twilio'
 
-twilio_number = 'unique token from twilio'
-target_number = '+91'
+twilio_number = 'enter unique token from twilio'
+target_number = '+91 must preceed'
 
